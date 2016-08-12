81d3884c549f2f5acac9ae06c6ebd987db278334
key <key@yahoo.com.tw>
2016-08-12 17:28:22 +0800

    2016/8/12

--- app/src/main/java/tw/com/key214kimo/rotoolbox/MainActivity.java
+++ app/src/main/java/tw/com/key214kimo/rotoolbox/MainActivity.java
@@ -1,8 +1,8 @@
 package tw.com.key214kimo.rotoolbox;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,11 +25,6 @@ public class MainActivity extends AppCompatActivity {
     public void click3(View v)
     {
         Intent it = new Intent(MainActivity.this, skill.class);//Main2Activity為指定的位置
-        startActivityForResult(it, 99);//設定兩個值
-    }
-    public void click4(View v)
-    {
-        Intent it = new Intent(MainActivity.this, Timer.class);//Main2Activity為指定的位置
         startActivityForResult(it, 99);//設定兩個值
     }
     public void click5(View v)
